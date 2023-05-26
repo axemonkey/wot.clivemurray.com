abe1bdf20a7fb72935fcd43324d7d202de09b2f5
axemonkey <hi+github@clivemurray.com>
2023-05-26 17:01:11 +0100

    lots of stuff around entering custom things

--- wot.clivemurray.com/assets/js/main.js
+++ wot.clivemurray.com/assets/js/main.js
@@ -1,10 +1,17 @@
 /*
 TODO:
-* max 20 things
+*** MAIN PAGE
+* link to "edit things", prepopulate the things form
+
+*** FORM PAGE
+* when loading, populate things from wheel
+* clear things - reset to 2 blank things
 * maxlength of things
-* add things to form
-* remove things from form
+* validation - no empty things
 */
+
+const minThings = 2;
+const maxThings = 20;
 
 const defaultThings = [
 	['Pizza', 'Burger', 'Thai', 'Fryup', 'Curry'],
@@ -280,12 +287,83 @@ const toggle = () => {
 };
 
 const submitThings = () => {
-	const thingEntries = document.querySelectorAll('.thingEntry input');
+	const thingEntries = document.querySelectorAll('.thing-entry input');
 	const values = [];
 	for (const thingEntry of thingEntries) {
 		values.push(thingEntry.value);
 	}
 	document.location.href = `/?things=${values.join(',')}`;
+};
+
+const addThing = () => {
+	const allThings = document.querySelector('.all-things');
+	const theThings = allThings.querySelectorAll('.thing-entry');
+	const currentNumberOfThings = theThings.length;
+
+	if (currentNumberOfThings < maxThings) {
+		// add thing
+		const newThingDiv = document.createElement('div');
+		const newThingLabel = document.createElement('label');
+		const newThingInput = document.createElement('input');
+		const newThingRemoveButton = document.createElement('button');
+
+		newThingDiv.classList.add('thing-entry');
+		newThingLabel.setAttribute('for', `thing${currentNumberOfThings + 1}`);
+		newThingLabel.innerHTML = `Thing ${currentNumberOfThings + 1}`;
+		newThingInput.setAttribute('type', 'text');
+		newThingInput.setAttribute('name', `thing${currentNumberOfThings + 1}`);
+		newThingInput.id = `thing${currentNumberOfThings + 1}`;
+		newThingInput.value = `Thing ${currentNumberOfThings + 1}`;
+		newThingRemoveButton.classList.add('removeThing');
+		newThingRemoveButton.classList.add('form-button');
+		newThingRemoveButton.innerHTML = 'Remove this thing';
+
+		newThingDiv.append(newThingLabel);
+		newThingDiv.append(newThingInput);
+		newThingDiv.append(newThingRemoveButton);
+
+		allThings.append(newThingDiv);
+	} else {
+		// can't add thing
+		console.log(`couldn't add thing`);
+	}
+};
+
+const renumberThings = () => {
+	const allThings = document.querySelector('.all-things');
+	const theThings = allThings.querySelectorAll('.thing-entry');
+	const currentNumberOfThings = theThings.length;
+
+	for (let index = 0; index < currentNumberOfThings; index++) {
+		const theThing = theThings[index];
+		const theThingLabel = theThing.querySelector('label');
+		const theThingInput = theThing.querySelector('input');
+
+		console.log(theThing);
+
+		theThingLabel.setAttribute('for', `thing${index + 1}`);
+		theThingLabel.innerHTML = `Thing ${index + 1}`;
+		theThingInput.setAttribute('name', `thing${index + 1}`);
+		theThingInput.id = `thing${index + 1}`;
+	}
+};
+
+const removeThing = thingButton => {
+	console.log(minThings);
+	console.log(thingButton.parentNode);
+
+	const allThings = document.querySelector('.all-things');
+	const theThings = allThings.querySelectorAll('.thing-entry');
+	const currentNumberOfThings = theThings.length;
+
+	if (currentNumberOfThings > minThings) {
+		const thingEntry = thingButton.parentNode;
+		thingEntry.parentNode.removeChild(thingEntry);
+		renumberThings();
+	} else {
+		// can't remove thing
+		console.log(`couldn't remove thing`);
+	}
 };
 
 // events
@@ -310,9 +388,24 @@ if (document.querySelector('#wheelHolder')) {
 	});
 }
 
-if (document.querySelector('#enterThings')) {
+if (document.querySelector('#enter-things')) {
 	document.querySelector('#go').addEventListener('click', event => {
 		event.preventDefault();
 		submitThings();
+	});
+	document.querySelector('#cancel').addEventListener('click', event => {
+		event.preventDefault();
+		window.history.go(-1);
+	});
+	document.querySelector('#addThing').addEventListener('click', event => {
+		event.preventDefault();
+		addThing();
+	});
+
+	document.addEventListener('click', event => {
+		if (event.target.classList.contains('removeThing')) {
+			event.preventDefault();
+			removeThing(event.target);
+		}
 	});
 }
