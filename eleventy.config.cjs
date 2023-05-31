efebb873a8409874e9c2a273cb6edf16b9c76c2f
axemonkey <hi+github@clivemurray.com>
2023-05-31 14:53:09 +0100

    fix asset watching

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,3 +1,11 @@
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('wot.clivemurray.com/public');
+	eleventyConfig.setUseGitIgnore(false);
+	eleventyConfig.setServerOptions({
+		liveReload: false,
+		watch: [
+			'wot.clivemurray.com/public/**/*',
+		],
+		showVersion: true,
+	});
 };
