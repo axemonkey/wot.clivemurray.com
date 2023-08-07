2f2dae6e13e581fbbc2e4cfb35735a835cf2a2f1
axemonkey <hi+github@clivemurray.com>
2023-08-07 14:47:51 +0100

    add robots.txt

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,5 +1,6 @@
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('wot.clivemurray.com/public');
+	eleventyConfig.addPassthroughCopy({ 'wot.clivemurray.com/robots.txt': '/robots.txt' });
 	eleventyConfig.setUseGitIgnore(false);
 	eleventyConfig.setServerOptions({
 		// liveReload: false,
