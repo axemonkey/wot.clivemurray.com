e80089666b2fdece9f26f7cc9046fe076c180c2f
axemonkey <hi+github@clivemurray.com>
2024-03-11 12:07:12 +0000

    update banner

--- _site/public/js/es5bundle.main.js
+++ _site/public/js/es5bundle.main.js
@@ -184,7 +184,7 @@
 
 	const lapBanner = {
 	  storageKey: 'loveAndPainkillers-banner-config',
-	  numberOfDaysToHideBanner: 2,
+	  numberOfDaysToHideBanner: 7,
 	  init: () => {
 	    // if not cookie
 	    if (lapBanner.cookieCheck()) {
@@ -204,8 +204,8 @@
 	  isCookieStillGood: cookieDismissed => {
 	    const now = Date.now(); // ms since epoch to now
 	    const since = now - cookieDismissed;
-	    const msInTwoDays = 1000 * 60 * 60 * 24 * lapBanner.numberOfDaysToHideBanner;
-	    if (since < msInTwoDays) {
+	    const daysToHideBannerInMs = 1000 * 60 * 60 * 24 * lapBanner.numberOfDaysToHideBanner;
+	    if (since < daysToHideBannerInMs) {
 	      return true;
 	    }
 	    return false;
@@ -228,18 +228,18 @@
 	  },
 	  assembleStyles: () => {
 	    let str = '';
-	    str += '.c-lap-banner {width: 100%;position: fixed;left: 0;bottom: 0;background: rgba(0, 0, 0, 90%); padding-top: 40px; @media (min-width: 600px) {padding-top: 0;} }';
+	    str += '.c-lap-banner {z-index: 9999; width: 100%;position: fixed;left: 0;bottom: 0;background: rgba(0, 0, 0, 90%); padding-top: 40px; @media (min-width: 600px) {padding-top: 0;} }';
 	    str += '.c-lap-banner * { font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol" !important; font-size: 22px !important; }';
 	    str += '.c-lap-banner strong {font-weight: bold;}';
-	    str += '.c-lap-banner p {color: #fff; margin-bottom: 1em;}';
+	    str += '.c-lap-banner p {color: #fff; margin-bottom: 1em;line-height: normal !important;}';
 	    str += '.c-lap-banner-inner {max-width: 800px; margin: auto; display: flex; flex-direction: row; flex-wrap: wrap; justify-content: center; align-items: center; gap: 20px;}';
 	    str += '.c-lap-banner__copy {padding: 10px 0; /*background: rgba(255, 0, 0, 50%);*/}';
 	    str += '.c-lap-banner-inner img {display: block; aspect-ratio: 1 / 1; width: 150px; height: 150px; flex-shrink: 0; overflow: hidden;}';
 	    str += '.c-lap-banner__headline {padding: 0 10px; letter-spacing: 1px; text-shadow: 0 0 4px rgba(92, 219, 255, 80%); @media (min-width: 600px) {padding: 0;} }';
 	    str += '.c-lap-banner__link {text-align: center;}';
-	    str += '.c-lap-banner__link a {text-decoration: none; color: #000; background-color: #5cdbff; border-radius: 100px; display: inline-block; padding: 7px 15px 5px; font-weight: bold; box-shadow: 0 0 4px rgba(255, 255, 255, 80%);}';
+	    str += '.c-lap-banner__link a {text-decoration: none; color: #000; font-style: normal; background-color: #5cdbff; border-radius: 100px; display: inline-block; padding: 7px 15px 5px; font-weight: bold; box-shadow: 0 0 4px rgba(255, 255, 255, 80%);}';
 	    str += '.c-lap-banner__link a:hover {background: #fff;}';
-	    str += '.c-lap-banner button {opacity: 1 !important;}';
+	    str += '.c-lap-banner button {opacity: 1 !important; letter-spacing: normal !important;}';
 	    str += '.c-lap-banner-closer {position: absolute;right: 5px;top: 5px; border: 0px solid transparent; border-radius: 100px;padding: 0 8px 2px; background: #5cdbff; color: #000; cursor: pointer;}';
 	    str += '.c-lap-banner-closer:hover {background: #fff;}';
 	    str += '.c-lap-banner-fadeout { animation: lpb-fade 1s forwards; }';
